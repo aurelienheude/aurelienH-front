2d40a8ec2ed652f197c57c1d53c4d99d20277f19
Aurélien Heude <aurelienheudepro@gmail.com>
2022-04-01 14:50:20 +0200

    test

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,16 +1,5 @@
-let burger = document.getElementById('burger');
+const burger = document.querySelector(".burger");
 
-let menu = document.getElementById('menu');
-let topbar = document.getElementById('topbar');
-let grid = document.getElementById('grid');
-
-burger.addEventListener('click', () =>{
-
-    burger.classList.toggle('active');
-    
-    if(burger.classList.contains('active')){
-        grid.style.gridTemplateColumns = "80px 1fr";
-    }else{
-        grid.style.gridTemplateColumns = "250px 1fr";
-    }
-})
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+});
