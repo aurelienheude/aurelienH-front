701feb9e8b4ceeaf4be05b3ae917c130d5d228b7
Aurélien Heude <aurelienheudepro@gmail.com>
2022-04-01 16:26:10 +0200

    update menu fini !

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,5 +1,20 @@
 const burger = document.querySelector(".burger");
+let burgerMenu = document.querySelector(".navBurger");
+let menu = document.querySelector(".nav");
 
-burger.addEventListener('click', () => {
+burger.addEventListener('click', e => {
   burger.classList.toggle('active');
+
+    burgerMenu.style.display = "flex";
+    menu.style.zIndex = "4";
+    menu.style.position = "absolute";
+    menu.style.width = "100%";
+
+    if(burger.classList.contains("active") == false)
+    {
+      burgerMenu.style.display = "none";
+      menu.style.zIndex = "1";
+      menu.style.position = "relative";
+      menu.style.width = "100%";
+    }
 });
